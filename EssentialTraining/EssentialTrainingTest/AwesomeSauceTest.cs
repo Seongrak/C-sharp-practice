905def5aa17997bf736020a3d71a3cd21ae84be6
rocky <rock9247@naver.com>
2021-01-22 15:10:59 -0500

    Essential Training commit

---
+++ EssentialTraining/EssentialTrainingTest/AwesomeSauceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTest
+{
+    [TestClass]
+    public class AwesomeSauceTest
+    {
+        [TestMethod]
+        public void IsSauceAwesomeTest()
+        {
+            var testInstance = new AwesomeSauce();
+            testInstance.Sauces.Add("Tobasco");
+            testInstance.Sauces.Add("Cholula");
+
+            //expect true
+            Assert.IsTrue(testInstance.IsSauceAwesome("Tobasco"));
+
+            //expect false
+            Assert.IsFalse(testInstance.IsSauceAwesome("Ketchup"));
+
+        }
+    }
+}
